d3ee5d7ca8ecdcd04a25b6cd582a793011bf0271
Nam <namhoang.td09@gmail.com>
2018-02-26 20:24:35 +0700

    Update  vocabulary

--- db/export_vocabulary.py
+++ db/export_vocabulary.py
@@ -11,30 +11,15 @@ def export_vocabulary(topic):
     word_count = Counter(topic_word)
     word_count = dict(word_count.items())
 
-    print(len(word_count))
-    # print(word_count)
-
-    # for pair in sorted(word_count, key=lambda k: k[1]) :
-    #     print pair
-
-
     bigrams = []
     for i in range(len(topic_word) - 1):
         bigrams.append(topic_word[i:i+2])
 
-    # print(bigrams[:10])
-
     str_bigrams = [' '.join(x) for x in bigrams]
-    # print(str_bigrams[:10])
-    # print(len(str_bigrams))
 
     gram_count = Counter(str_bigrams)
 
-    print(len(list(gram_count.items())))
-    print(gram_count.most_common(10))
-
     pair_count = {}
-
 
     for x in list(gram_count.items()):
         pair = x[0].split()
@@ -46,16 +31,15 @@ def export_vocabulary(topic):
         else:
             pair_count[pair[0]] = {'follower': pair[1], 'count': x[1]}
 
-    print(pair_count['artificial'])
-    print(len(list(pair_count.items())))
-    print(list(pair_count.items())[:10])
-
     with open('vocabulary/' + topic + '.csv', 'wb') as outcsv:
         writer = csv.writer(outcsv)
         writer.writerow(["Word", "Follower", "Count"])
 
         for pair in list(pair_count.items()):
-            writer.writerow([pair[0].encode('utf-8'), pair[1]["follower"].encode('utf-8'), word_count[pair[0]]])
+            word = pair[0].encode('utf-8')
+            word = word.strip()
+            if word.isalpha():
+                writer.writerow([word, pair[1]["follower"].encode('utf-8'), word_count[pair[0]]])
 
 
 
